709937878bcb9a19af9ad6430d97fffaeefb8e19
njdevengine <newjerseydevelopment@gmail.com>
2020-03-02 17:14:47 -0500

    Update tweets.py

--- tweets.py
+++ tweets.py
@@ -27,3 +27,38 @@ for i in tweets:
         
 tweet_string = " ".join(clean_tweets)
 tokens = word_tokenize(tweet_string)
+text = nltk.Text(tokens)
+
+#collocations = text.collocations()
+#text.similar("Trump")
+
+import nltk
+from nltk.book import *
+fdist = FreqDist(text)
+
+#fdist.most_common(10)
+
+my_set = set(text)
+long_words = [word for word in my_set if len(word) > 15]
+#sorted(long_words)
+
+filtered_array = sorted(set(word.lower() for word in text if word.isalpha() and len(word)>3))
+
+tags = []
+hashtags = []
+for i in range(len(clean_tweets)):
+    if "@" in clean_tweets[i]:
+        for n in clean_tweets[i].split(" "):
+            if "@" in n:
+                tags.append(n)
+                
+for i in range(len(clean_tweets)):
+    if "#" in clean_tweets[i]:
+        for n in clean_tweets[i].split(" "):
+            if "#" in n:
+                hashtags.append(n)     
+                
+tag_df = pd.DataFrame({"tag":tags})
+hash_df = pd.DataFrame({"hash":hashtags})
+tag_df.to_csv("tag.csv")
+hash_df.to_csv("hash.csv")
