991bb319fc17bb1712f67a1fbdbcf8c286ddc13f
njdevengine <newjerseydevelopment@gmail.com>
2020-03-02 16:01:10 -0500

    Update tweets.py

--- tweets.py
+++ tweets.py
@@ -1,5 +1,8 @@
 from os import listdir
 from os.path import isfile, join
+import nltk, re, pprint
+from nltk import word_tokenize
+import pandas as pd
 
 mypath = "russian-troll-tweets-master"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
@@ -11,5 +14,16 @@ for i in files:
     df = pd.read_csv(i)
     frames.append(df)
 
-import pandas as pd
 df = pd.concat(frames)
+
+df2 = df[df.language =="English"]
+
+tweets = list(set(list(df2.content)))
+
+clean_tweets = []
+for i in tweets:
+    if type(i) != float:
+        clean_tweets.append(i)
+        
+tweet_string = " ".join(clean_tweets)
+tokens = word_tokenize(tweet_string)
